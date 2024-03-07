d8b214850be99835c141f0ebac9d7ee7782859b3
HuysGithub <august16.hn@gmail.com>
2024-03-07 23:44:59 +0700

    TungML

---
+++ config.js
@@ -0,0 +1,13 @@
+const nameGirl = 'Diệu linh';
+const giftUrl = '';
+const eventName = 'Chúc Mừng 8/3';
+const titleCard = 'Gửi Tặng Diệu Linh Xinh Gái';
+const contentCard = 'Chúc Diệu Linh có một ngày mùng 8/3 tràn ngập những niềm vui và nụ cười nhá. Hãy luôn giữ nụ cười trên môi cậu nhé bởi nụ cười của cậu còn đẹp hơn tất cả những bông hoa trong ngày mùng 8 tháng 3 này á';
+
+// phần dưới dành cho các bạn biết code, nếu muốn chỉnh ảnh đơn giản với base64
+// Cần hỗ trợ hãy liên hệ: 
+// Mr-Nam http://facebook.com/nam.nodemy
+// Các bạn muốn học lập trình thì tham gia Nhóm zalo tự học lập trình nhé: https://zalo.me/g/yhdkef092
+const giftImage = 'hot-girl.png';
+const base64 = '';
+const giftImageBase64 = "data:image/png;base64, " + base64;
